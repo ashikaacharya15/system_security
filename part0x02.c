84243ecf64671ed2b0496a9698e4038ed081e625
Ashika Acharya <53186503+ashikaacharya15@users.noreply.github.com>
2020-11-05 17:03:19 -0800

    Add files via upload

---
+++ part0x02.c
@@ -0,0 +1,103 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <limits.h>
+
+#include "macros.h"
+#include "ia32_context.h"
+
+/* addresses of asm callout glue code */
+
+extern void* retCallout;
+
+/*********************************************************************
+ *
+ *  callout handler
+ *
+ *   These get called by asm glue routines.
+ *
+ *********************************************************************/
+
+void
+handleRetCallout(SaveRegs regs)
+{
+   printf("inside return callout\n");
+}
+
+int fib(int i);
+
+/*********************************************************************
+ *
+ *  StartProfiling, StopProfiling
+ *
+ *   Profiling hooks. This is your place to inspect and modify the profiled
+ *   function.
+ *
+ *********************************************************************/
+
+void
+StartProfiling(void *func)
+{
+    uint8_t *func_ptr;
+    func_ptr = (uint8_t *) func;
+
+    uint8_t *func_ret;
+    func_ret = (uint8_t *) &retCallout;
+
+    printf("%p\n", func_ptr);
+    printf("%p\n", func_ret);
+    int offset = func_ret - func_ptr;
+    printf("%x\n", offset);
+
+    func_ptr[0] = 0xe8;
+    func_ptr[1] = 0x3d;
+    func_ptr[2] = 0x00;
+    func_ptr[3] = 0x00;
+    func_ptr[4] = 0x00;
+}
+
+
+int main(int argc, char *argv[])
+{
+   int value;
+   char *end;
+
+   if (argc != 2) {
+      fprintf(stderr, "usage: %s <value>\n", argv[0]);
+      exit(1);
+   }
+
+   value = strtol(argv[1], &end, 10);
+
+   if (((errno == ERANGE)
+        && ((value == LONG_MAX) || (value == LONG_MIN)))
+       || ((errno != 0) && (value == 0))) {
+      perror("strtol");
+      exit(1);
+   }
+
+   if (end == argv[1]) {
+      fprintf(stderr, "error: %s is not an integer\n", argv[1]);
+      exit(1);
+   }
+
+   if (*end != '\0') {
+      fprintf(stderr, "error: junk at end of parameter: %s\n", end);
+      exit(1);
+   }
+
+   StartProfiling(fib);
+
+   value = fib(value);
+
+   printf("%d\n", value);
+   exit(0);
+}
+
+int fib(int i)
+{
+   if (i <= 1) {
+      return 1;
+   }
+   return fib(i-1) + fib(i-2);
+}
